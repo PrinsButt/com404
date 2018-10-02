f1e6ead729549b0ccf1d7c825b5ab146d304709d
Prins <prinsbutt@gmail.com>
2018-10-02 16:08:05 +0100

    Create 4-string-multiply/bot.py
    Added code to demonstrate how to multiply strings.

---
+++ 1-basics/2-input/4-string-multiply/bot.py
@@ -0,0 +1,13 @@
+# Display strings with operators
+print("Please enter number of lives")
+lives = int(input())
+
+print("Please enter energy level")
+energy = int(input())
+
+print("Please enter shield level")
+shield = int(input()) 
+
+print("Lives:", "♥" * lives)
+print("Energy:", "♦" * energy)
+print("Shield:", "♦" * shield)

d56f77c09394d7eeab638acea73e92c0fa5f9f90
Prins <prinsbutt@gmail.com>
2018-10-02 16:38:49 +0100

    Create 1-if/bot.py
    Added code to demonstrate the use of an if statement.

---
+++ 1-basics/3-decision/1-if/bot.py
@@ -0,0 +1,11 @@
+# Read direction from user 
+print("Please enter one of the following:")
+print("...w to move up")
+print("...s to move down")
+print("...a to move left")
+print("...d to move right")
+
+direction = input()
+
+if (direction == "w"):
+  print("I will move up.")

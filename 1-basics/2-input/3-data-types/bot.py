97a688c2a552281094cd7aa5fdd885678c52fa18
Prins <prinsbutt@gmail.com>
2018-10-02 15:58:06 +0100

    Create 3-data-types/bot.py
    Added code to read different data types.

---
+++ 1-basics/2-input/3-data-types/bot.py
@@ -0,0 +1,16 @@
+# Read in user data 
+print("What is your name?")
+name = str( input() ) 
+
+print("What is your age?")
+age = int( input() )
+
+print("What is your weight?") 
+weight = float( input() ) 
+
+print("What is your height?")
+height = float( input() )
+
+bmi = weight / (height * height)
+
+print(name, "your bmi is", bmi)

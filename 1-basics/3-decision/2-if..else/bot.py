335b06790402e40e54798b8ee2bd3bc4cd37af0c
Prins <prinsbutt@gmail.com>
2018-10-02 16:37:43 +0100

    Create 2-if..else/bot.py
    Added code to demonstrate the use of if...else.

---
+++ 1-basics/3-decision/2-if..else/bot.py
@@ -0,0 +1,13 @@
+# Read direction from user 
+print("Please enter one of the following:")
+print("...w to move up")
+print("...s to move down")
+print("...a to move left")
+print("...d to move right")
+
+direction = input()
+
+if (direction == "w"):
+  print("I will move up.")
+else:
+  print("Error! Please specify again.")
